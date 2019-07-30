06cafc032bf772d1b8bfb23dd4d53bcce7d7c337
Krzysztof Pobożan <krzpob@gmail.com>
2019-07-30 20:57:36 +0200

    start springboot

---
+++ src/main/java/pl/javasoft/springboot2keycloack/SpringBoot2KeycloackApplication.java
@@ -0,0 +1,13 @@
+package pl.javasoft.springboot2keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2KeycloackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2KeycloackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javasoft/springboot2keycloack/SpringBoot2KeycloackApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.javasoft.springboot2keycloack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2KeycloackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
